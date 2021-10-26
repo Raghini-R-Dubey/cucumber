4825751e90bab58f90260763978a3eaebccc0bf6
Raghini-R-Dubey <raghini@angularminds.in>
2021-10-26 16:15:20 +0530

    runner class

---
+++ testrunner.java
@@ -0,0 +1,20 @@
+package stepdefinitions;
+
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.Cucumber;
+@RunWith(Cucumber.class)
+@CucumberOptions(features="src/test/resources/Features",glue= {"stepdefinitions"}, monochrome= true,
+plugin={"pretty","junit:target/JUnitReports/reports.xml",
+		"json:target/JSONReports/reports.json",
+		"html:target/HtmlReports/reports.html"},
+tags="@smoketest")
+
+public class testrunner {
+
+}
+
+
+
